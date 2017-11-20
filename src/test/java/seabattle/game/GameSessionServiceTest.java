c5b7e326e474bdb38df17cfb3c63cf503a62c071
vla-parfenkov <vlaparfenkov@gmail.com>
2017-11-20 17:21:10 +0300

    fix test

--- src/test/java/seabattle/game/GameSessionServiceTest.java
+++ src/test/java/seabattle/game/GameSessionServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
 import seabattle.game.gamesession.GameSessionService;
 import seabattle.game.messages.MsgLobbyCreated;
@@ -14,9 +13,9 @@ import seabattle.websocket.WebSocketService;
 
 import java.io.IOException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -33,18 +32,16 @@ public class GameSessionServiceTest {
     private WebSocketService webSocketService;
 
     private void createGameSession() {
-        final MsgLobbyCreated initMessage1 = new MsgLobbyCreated(player1.getUsername());
-        final MsgLobbyCreated initMessage2 = new MsgLobbyCreated(player2.getUsername());
         try {
-            doNothing().when(webSocketService).sendMessage(eq(player1.getPlayerId()), eq(initMessage2));
-            doNothing().when(webSocketService).sendMessage(eq(player2.getPlayerId()), eq(initMessage1));
+            doNothing().when(webSocketService).sendMessage(eq(player1.getPlayerId()), any(MsgLobbyCreated.class));
+            doNothing().when(webSocketService).sendMessage(eq(player2.getPlayerId()), any(MsgLobbyCreated.class));
         } catch (IOException ignore) {
 
         }
         gameSessionService.createSession(player1, player2);
         try {
-            verify(webSocketService).sendMessage(eq(player2.getPlayerId()), eq(initMessage1));
-            verify(webSocketService).sendMessage(eq(player1.getPlayerId()), eq(initMessage2));
+            verify(webSocketService).sendMessage(eq(player2.getPlayerId()), any(MsgLobbyCreated.class));
+            verify(webSocketService).sendMessage(eq(player1.getPlayerId()), any(MsgLobbyCreated.class));
         } catch (IOException ignore) {
         }
 
