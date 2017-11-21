5fd13a855e3f71d7a326aa65d4fac9df58eee9f1
vla-parfenkov <vlaparfenkov@gmail.com>
2017-11-22 02:45:50 +0300

    fix msgYouinQueue 2

--- src/main/java/seabattle/game/messages/MsgYouInQueue.java
+++ src/main/java/seabattle/game/messages/MsgYouInQueue.java
@@ -12,6 +12,14 @@ public class MsgYouInQueue extends Message {
         this.nickname = nickname;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNickname() {
         return nickname;
     }
